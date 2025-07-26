a6183f3aa82d2ff110d698d6f84a14fb85c0f536
John Su <Johnchangsu@gmail.com>
2025-07-27 09:26:10 +1000

    Added returns to array ops to make code more readable. Deprecate once we move to warp 1.8.0+

--- warp_cfd/FV/Ops/array_ops.py
+++ warp_cfd/FV/Ops/array_ops.py
@@ -70,39 +70,39 @@ def add_1D_array(a:wp.array,b:wp.array,c:wp.array):
     assert len(a.shape) == 1 and len(b.shape) == 1 and len(c.shape) == 1
 
     wp.launch(kernel=add_1D_array_kernel,dim = a.shape[0],inputs=[a,b,c])
-
+    return c
 def sub_1D_array(a:wp.array,b:wp.array,c:wp.array):
     '''subtract b from a element wise into array c i.e. `c[i] = a[i] - b[i]`'''
     assert a.shape[0] == b.shape[0] and c.shape[0] == b.shape[0]
     assert len(a.shape) == 1 and len(b.shape) == 1 and len(c.shape) == 1
 
     wp.launch(kernel=sub_1D_array_kernel,dim = a.shape[0],inputs=[a,b,c])
-
+    return c
 def mult_1D_array(a:wp.array,b:wp.array,c:wp.array):
     '''multiply a and b element wise into array c i.e. `c[i] = a[i] * b[i]`'''
     assert a.shape[0] == b.shape[0] and c.shape[0] == b.shape[0]
     assert len(a.shape) == 1 and len(b.shape) == 1 and len(c.shape) == 1
 
     wp.launch(kernel=mult_1D_array_kernel,dim = a.shape[0],inputs=[a,b,c])
-
+    return c
 def div_1D_array(a:wp.array,b:wp.array,c:wp.array):
     '''divide b from a element wise into array c i.e. `c[i] = a[i] / b[i]`'''
     assert a.shape[0] == b.shape[0] and c.shape[0] == b.shape[0]
     assert len(a.shape) == 1 and len(b.shape) == 1 and len(c.shape) == 1
 
     wp.launch(kernel=div_1D_array_kernel,dim = a.shape[0],inputs=[a,b,c])
-
+    return c
 def inv_1D_array(a:wp.array,b:wp.array):
     '''invert elemens in a into array b i.e. `b[i] = 1/a[i]`'''
     assert a.shape[0] == b.shape[0] and len(a.shape) == 1
     wp.launch(kernel=inv_1D_array_kernel,dim = a.shape[0],inputs=  [a,b])
-
+    return b
 def mult_scalar_1D_array(a:wp.array,scalar:float,c:wp.array):
     '''multiply b array with scalar into array c i.e. `c[i] = a[i]*scalar`'''
     assert a.shape[0] == c.shape[0]
     assert len(a.shape) == 1
     wp.launch(kernel=mult_scalar_1D_array_kernel,dim = a.shape[0],inputs=[a,scalar,c])
-    
+    return c
 
 def add_scalar_1D_array(a:wp.array,scalar:float,c:wp.array):
     '''add b array with scalar into array c i.e. `c[i] = a[i] + scalar`'''
@@ -110,25 +110,25 @@ def add_scalar_1D_array(a:wp.array,scalar:float,c:wp.array):
     assert len(a.shape) == 1
 
     wp.launch(kernel=add_scalar_1D_array_kernel,dim = a.shape[0],inputs=[a,scalar,c])
-    
+    return c
 def power_scalar_1D_array(a:wp.array,scalar:float,b:wp.array):
     assert a.shape[0] == b.shape[0]
     assert len(a.shape) == 1
     wp.launch(kernel=power_scalar_1D_array_kernel,dim = a.shape[0],inputs=[a,scalar,b])
-
+    return b
 
 def absolute_1D_array(a:wp.array,b:wp.array):
     assert a.shape[0] == b.shape[0]
     assert len(a.shape) == 1
     
     wp.launch(kernel=absolute_1D_array_kernel,dim = a.shape[0],inputs=[a,b])
-
+    return b
 def sum_1D_array(a:wp.array) -> float: 
     '''Sum up elements in array and return total as float'''
     c = wp.zeros(1,dtype=a.dtype)
     wp.launch(kernel=naive_sum_1D_array_kernel,dim = a.shape[0], inputs = [a])
     return c.numpy()[0]
-
+    
 
 def L_norm_1D_array(a:wp.array,power:float,reduction = None):
     b = wp.empty_like(a)
--- warp_cfd/FV/implicit_Solvers/SIMPLE.py
+++ warp_cfd/FV/implicit_Solvers/SIMPLE.py
@@ -4,19 +4,19 @@ import numpy as np
 from warp_cfd.FV.terms import ConvectionTerm,DiffusionTerm, GradTerm,Matrix
 from warp_cfd.FV.field import Field
 import warp as wp
-from warp_cfd.FV.Ops.array_ops import sub_1D_array
+from warp_cfd.FV.Ops.array_ops import sub_1D_array,add_1D_array,div_1D_array
 
 class SIMPLE():
-    def __init__(self,model:FVM,u_relaxation_factor=0.7,p_relaxation_factor = 0.3) -> None:
+    def __init__(self,model:FVM,u_relaxation_factor=0.7,p_relaxation_factor = 0.3,correction = False) -> None:
         self.model = model
         velocity_vars = ['u','v','w']
         self.convection = ConvectionTerm(model,velocity_vars,'upwind') # We only want velocities
-        self.diffusion = DiffusionTerm(model,velocity_vars,correction = True)
+        self.diffusion = DiffusionTerm(model,velocity_vars,correction = correction)
         self.grad_P = GradTerm(model,'p') # P
 
         self.vel_equation = Matrix(model,fields = velocity_vars)
 
-        self.p_correction_diffusion = DiffusionTerm(model,'p_cor',need_global_index= True,von_neumann= 0.,correction=True)
+        self.p_correction_diffusion = DiffusionTerm(model,'p_cor',need_global_index= True,von_neumann= 0.,correction=correction)
         self.p_corr_equation = Matrix(model,fields = 'p_cor')
 
         self.vel_correction = wp.zeros(shape=(model.num_cells,3),dtype=float)
@@ -25,7 +25,10 @@ class SIMPLE():
 
         self.p_relaxation_factor = p_relaxation_factor
         self.u_relaxation_factor = u_relaxation_factor
-        self.NUM_INNER_LOOPS = 1
+        self.NUM_INNER_LOOPS = 3
+
+        self.HbyA = wp.zeros_like(self.vel_array)
+        self.grad_P_HbyA = wp.zeros_like(self.vel_array)
 
     def run(self,num_steps,steps_per_check = 10,*,rhie_chow = True):
         model = self.model
@@ -36,14 +39,10 @@ class SIMPLE():
         vel_equation= self.vel_equation
         p_corr_equation = self.p_corr_equation
         vel_array = self.vel_array
-
+        HbyA = self.HbyA
         for i in range(num_steps):
             model.face_interpolation()
-            internal_ids = model.face_properties.internal_face_ids.numpy()
-            # print(internal_ids)
-            # print(model.face_values.numpy()[[20,26,25,28],0])
-            # print(model.face_properties.centroid.numpy()[[20,26,25,28]])
-            # print(model.cell_values.numpy())
+            
             model.calculate_gradients()
             model.calculate_mass_flux(rhie_chow=rhie_chow)
 
@@ -52,41 +51,52 @@ class SIMPLE():
             diffusion(model,viscosity = model.viscosity)
             grad_P(model)
             vel_equation.form_system([convection,-diffusion],explicit_terms= -grad_P,fvm = model)
-            
+            # print('u\n',vel_equation.dense[::3,::3])
             vel_equation.relax(self.u_relaxation_factor,model)
-            # print(vel_equation.dense[::3,::3])
-            ap = vel_equation.diagonal
+            # 
+            # print('v\n',vel_equation.dense[1::3,1::3])
+            Ap = vel_equation.diagonal
             outer_loop_result,vel_array = vel_equation.solve_Axb(vel_array)
             # print(vel_array[::3])
             
+            
             model.replace_cell_values([0,1,2],vel_array)
 
-            model.pressure_correction_ops.calculate_D_viscosity(model.D_cell,model.D_face,ap,model.cells,model.faces)
+            model.pressure_correction_ops.calculate_D_viscosity(model.D_cell,model.D_face,Ap,model.cells,model.faces)
 
-            model.face_interpolation()
-            model.calculate_gradients()
-            model.calculate_mass_flux(rhie_chow=rhie_chow)
+            grad_P_HbyA = div_1D_array(grad_P.weights,Ap,grad_P_HbyA)
+            HbyA = add_1D_array(vel_array,grad_P_HbyA,HbyA)
+            # Interpolate HbyA to Faces and get mass flux
+            # Add to RHS
 
-            # Pressure Correction
-            div_u = model.calculate_divergence()
-            
-            p_correction_diffusion(model,viscosity = model.D_face)
+            for _ in range(self.NUM_INNER_LOOPS):
+                model.face_interpolation()
+                model.calculate_gradients()
+                model.calculate_mass_flux(rhie_chow=rhie_chow)
 
-            p_corr_equation.form_system(p_correction_diffusion,fvm = model)
-            p_corr_equation.add_RHS(div_u)
-            # print(p_corr_equation.dense)
-            p_corr_equation.replace_row(0,0.)
+                # Pressure Correction
+                div_u = model.calculate_divergence()
+                
+                p_correction_diffusion(model,viscosity = model.D_face)
+
+                p_corr_equation.form_system(p_correction_diffusion,fvm = model)
+                p_corr_equation.add_RHS(div_u)
+                # print(p_corr_equation.dense)
+                p_corr_equation.replace_row(0,0.)
+                
+                inner_loop_result,p_cor = p_corr_equation.solve_Axb()
+                #Update Pressure
+            #     model.replace_cell_values(4,p_cor)
+                # print(model.cell_values.numpy()[:,3])
+                model.update_cell_values(3,p_cor,scale = self.p_relaxation_factor)
+                # print(model.cell_values.numpy()[:,3])
+                #Update Velocity
+                vel_correction = p_corr_equation.calculate_gradient(coeff=model.D_face,fvm = model)
+                sub_1D_array(vel_array,vel_correction.flatten(),vel_array)
+                model.replace_cell_values([0,1,2],vel_array)
             
-            inner_loop_result,p_cor = p_corr_equation.solve_Axb()
-            #Update Pressure
-        #     model.replace_cell_values(4,p_cor)
-            model.update_cell_values(3,p_cor,scale = self.p_relaxation_factor)
-            
-            #Update Velocity
-            vel_correction = p_corr_equation.calculate_gradient(coeff=model.D_face,fvm = model)
-            sub_1D_array(vel_array,vel_correction.flatten(),vel_array)
-            model.replace_cell_values([0,1,2],vel_array)
-            
+
+
             if i % steps_per_check == 0:
                 print(f'step iter: {i}')
                 print(f'Outer loop Linear Solve {outer_loop_result} Inner loop solve {inner_loop_result}:')
