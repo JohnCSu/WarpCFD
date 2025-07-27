ce59382822b7b75650160803b217938e74f11c82
John Su <Johnchangsu@gmail.com>
2025-07-27 10:02:28 +1000

    Removed Pressure correction ops import as now deprecated and not used

--- warp_cfd/FV/Ops/__init__.py
+++ warp_cfd/FV/Ops/__init__.py
@@ -1,4 +1,3 @@
 from warp_cfd.FV.Ops.mesh_ops import Mesh_Ops 
 from warp_cfd.FV.Ops.weight_ops import Weights_Ops
 from warp_cfd.FV.Ops.matrix_ops import Matrix_Ops
-from warp_cfd.FV.Ops.pressure_correction_ops import Pressure_correction_Ops
