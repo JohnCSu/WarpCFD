a7396a565228f4ddfc1632cd53ceb50d07f64f19
John Su <Johnchangsu@gmail.com>
2025-08-04 22:52:04 +1000

    Renamed matrix ops to equation ops to better reflect change to Equation object

--- warp_cfd/FV/Ops/equation_ops.py
+++ warp_cfd/FV/Ops/equation_ops.py
@@ -4,6 +4,8 @@ from warp_cfd.FV.utils import COO_Arrays
 from warp.optim import linear
 import warp.sparse as sparse
 from .ops_class import Ops
+from warp_cfd.FV.mesh_structs import CELL_DICT
+
 class Matrix_Ops(Ops):
     def __init__(self,cell_struct,face_struct,node_struct,cell_properties,face_properties,num_outputs,float_dtype = wp.float32,int_dtype = wp.int32):
         super().__init__(cell_struct,face_struct,node_struct,cell_properties,face_properties,num_outputs,float_dtype,int_dtype)
@@ -273,3 +275,185 @@ class Matrix_Ops(Ops):
 
 
 
+@wp.kernel
+def calculate_BSR_matrix_indices( rows:wp.array(dtype=int),
+                        cols:wp.array(dtype=int),
+                        offsets:wp.array(dtype=int),
+                        cell_structs: wp.array(dtype=Any),
+                        face_structs:wp.array(dtype=Any),
+                        num_outputs: int):
+    i,nnz_cell_offset,output = wp.tid() # Loop C,NNZ,D (set to 3)
+    D = num_outputs
+    cell_offset = offsets[i]
+    num_nnz_per_cell = cell_structs[i].nnz
+    # wp.printf('i:%d offset:%d output: %d \n',i,nnz_cell_offset,output)   
+    if nnz_cell_offset == 0: # We are weighing the diagonal
+        total_offset = cell_offset + (num_nnz_per_cell)*output
+        rows[total_offset] = D*i + output
+        #Because of structure, if 0 then we just record that the diagonal is needed. Updates to values is made when we iterate over face_idx
+        cols[total_offset] = rows[total_offset]
+        # wp.printf('i:%d offset:%d num_nnz_per_cell %d nnz_cell_offset:%d output:%d r:%d c:%d t:%d \n', i,cell_offset,num_nnz_per_cell,nnz_cell_offset,output,rows[total_offset],cols[total_offset],total_offset)  
+    else:
+        # Find the neighbor cell id corresponding to the face idx which gives us the column
+        face_idx = nnz_cell_offset - 1 # 0 is considered the diagonal (self)
+        face_id = cell_structs[i].faces[face_idx]
+        
+        if face_structs[face_id].is_boundary == 0: # Internal Face
+            face_offset = cell_structs[i].face_offset_index[face_idx]
+            total_offset = cell_offset + (num_nnz_per_cell)*output + face_offset # Need Face offset so for each face index from 1->(F+1)
+            rows[total_offset] = D*i + output
+            neighbor_cell_id = cell_structs[i].neighbors[face_idx]
+            cols[total_offset] = D*neighbor_cell_id + output
+
+
+
+
+@wp.func
+def get_face_idx(vec1:Any,id:int):
+    for i in range(vec1.length):
+        if vec1[i] == id:
+            return i
+    
+    return -1
+
+
+@wp.kernel
+def calculate_BSR_values_kernel(bsr_rows:wp.array(dtype=int),
+                        bsr_columns:wp.array(dtype=int),
+                        values:wp.array(dtype= Any),
+                        cell_structs:wp.array(dtype= Any),
+                        weights:wp.array4d(dtype= Any),
+                        output_indices:wp.array(dtype= int),
+                        scale:Any):
+    i = wp.tid()
+
+    row = bsr_rows[i]
+    col = bsr_columns[i]
+    num_outputs = output_indices.shape[0]
+    output_idx = wp.mod(row,num_outputs)
+    output = output_indices[output_idx]
+    cell_id = row//num_outputs
+    neighbor_id = col//num_outputs
+
+    owner_cell = cell_structs[cell_id]
+
+    if row == col:
+        for face_idx in range(owner_cell.faces.length):
+            # if owner_cell.neighbors[face_idx] != -1: # So not boundary
+            wp.atomic_add(values,i, scale*weights[cell_id,face_idx,output,0])
+    else: # Off Diagonal
+        face_idx = get_face_idx(owner_cell.neighbors,neighbor_id)
+        if face_idx != -1:    
+            wp.atomic_add(values,i, scale*weights[cell_id,face_idx,output,1])
+
+
+
+@wp.kernel
+def _calculate_RHS_values_kernel(b:wp.array(dtype=Any),
+                                boundary_face_ids:wp.array(dtype= int),
+                                face_structs:wp.array(dtype=Any ),
+                                weights:wp.array4d(dtype= Any ),
+                                output_indices:wp.array(dtype= int),
+                                scale:Any):
+    
+    i,output_idx = wp.tid() #Loop through Boundary faces
+
+    face_id = boundary_face_ids[i]
+    cell_id = face_structs[face_id].adjacent_cells[0]
+    face_idx = face_structs[face_id].cell_face_index[0]
+    output = output_indices[output_idx]
+    row = cell_id*output_indices.shape[0] +output_idx
+
+    # wp.atomic_add(b,row,weights[cell_id,face_idx,output].neighbor -  weights[cell_id,face_idx,output].neighbor )
+    wp.atomic_add(b,row,-scale*weights[cell_id,face_idx,output,2] )
+
+
+
+@wp.kernel
+def implicit_relaxation_kernel(bsr_rows:wp.array(dtype=int),
+                        bsr_columns:wp.array(dtype=int),
+                        values:wp.array(dtype= Any),
+                        b:wp.array(dtype=Any),
+                        cell_values:wp.array2d(dtype=Any),
+                        alpha:Any,
+                        output_indices:wp.array(dtype=int)):
+    ''' Can be optimised to only take diag ahead of time'''
+    i = wp.tid()
+
+    row = bsr_rows[i]
+    col = bsr_columns[i]
+
+    if row == col: # Diag
+        # Row//num_outputs == Cell_Id ?
+        num_outputs = output_indices.shape[0]
+        output_idx = wp.mod(row,num_outputs)
+        
+        output = output_indices[output_idx]
+        cell_id = row//num_outputs
+        cell_value = cell_values[cell_id,output]
+        
+        b[row] +=  (type(alpha)(1.)-alpha)/alpha*values[i]*cell_value
+        values[i] = values[i]/alpha
+        
+
+@wp.kernel
+def replace_row_kernel(bsr_row_offsets:wp.array(dtype= int),
+                        bsr_columns: wp.array(dtype= int),
+                        bsr_values:wp.array(dtype=Any),
+                        b:wp.array(dtype=Any),
+                        row_ids:wp.array(dtype=int),
+                        rhs_values:wp.array(dtype=Any)):
+    
+    i = wp.tid() # We go through all Cells with fixed pressure values
+    
+    #For now only for pressure so cell_id = row
+    row_id = row_ids[i]
+    col_start,col_end = bsr_row_offsets[row_id], bsr_row_offsets[row_id+1]
+    for nnz_idx in range(col_start,col_end):
+        column = bsr_columns[nnz_idx]
+        if row_id == column: #Diagonal
+            bsr_values[nnz_idx] = bsr_values.dtype(1.)
+        else: # Off diagonal
+            bsr_values[nnz_idx] = bsr_values.dtype(0.)
+    
+    b[row_id] = rhs_values[row_id] # Replace the row with the specified value
+
+
+
+
+'''Overloads FOR FP ONLY'''
+
+for T in [wp.float32,wp.float64]:
+    wp.overload(implicit_relaxation_kernel,{"values":wp.array(dtype=T),
+                                            "b":wp.array(dtype=T),
+                                        "cell_values":wp.array2d(dtype=T),
+                                        "alpha":T
+                                         })
+    
+    wp.overload(replace_row_kernel,{"bsr_values":wp.array(dtype=T),
+                                            "b":wp.array(dtype=T),
+                                        "rhs_values":wp.array(dtype=T),
+                                         })
+    
+
+
+
+
+'''OVERLOADS WITH CELL STRUCTS'''
+
+for key,(cell_struct,face_struct,node_struct) in CELL_DICT.items():
+    float_type = wp.float32 if 'F32' in key else wp.float64
+    wp.overload(calculate_BSR_values_kernel,{"cell_structs":wp.array(dtype=cell_struct),
+                                    "values":wp.array(dtype= float_type),
+                                    "weights":wp.array4d(dtype= float_type),
+                                    "scale":float_type
+                                         })
+    wp.overload(_calculate_RHS_values_kernel,{   "face_structs":wp.array(dtype=face_struct),
+                                                "b":wp.array(dtype= float_type),
+                                                "weights":wp.array4d(dtype= float_type),
+                                                "scale":float_type
+                                                    })
+    
+    wp.overload(calculate_BSR_matrix_indices,{"cell_structs":wp.array(dtype=cell_struct),
+                                    "face_structs":wp.array(dtype=face_struct),
+                                        })
