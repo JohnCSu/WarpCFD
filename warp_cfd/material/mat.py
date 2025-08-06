062963830c74a120224f8f8463bd951200ad56b9
John Su <Johnchangsu@gmail.com>
2025-08-06 16:26:21 +1000

    added material Module

--- warp_cfd/material/mat.py
+++ warp_cfd/material/mat.py
@@ -2,12 +2,18 @@ class Material():
     def __init__(self):
         pass
 
+    def property_exists(self,variable:str):
+        if not hasattr(self,f'_{variable}'):
+            raise ValueError(f'{variable} has not been defined')
     @property
     def density(self):
+        self.property_exists('density')
         return self._density
     
     @property
     def viscosity(self):
+        '''Dynamic Viscosity'''
+        self.property_exists('viscosity')
         return self._viscosity
     
     def set_density(self,density,incompressible = True):
@@ -17,3 +23,7 @@ class Material():
     def set_viscosity(self,viscosity,newtonian = True):
         self.viscosity_type = 'newtonian' if newtonian else 'non-newtonian'
         self._viscosity = viscosity
+
+    def create_incompressible_newtonian_fluid(self,viscosity,density):
+        self.set_density(density) 
+        self.set_viscosity(viscosity)
